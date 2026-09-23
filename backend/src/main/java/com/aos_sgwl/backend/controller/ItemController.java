fb9c2dd5fb9fd19a314bb1c0828a18b1fc154274
Felipe-tecnico-cmyk <felipetecnico911@gmail.com>
2026-09-23 08:52:39 -0300

    Create ItemController.java

---
+++ backend/src/main/java/com/aos_sgwl/backend/controller/ItemController.java
@@ -0,0 +1,11 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class ItemController {
+    @GetMapping("/itens")
+    public 
+}
