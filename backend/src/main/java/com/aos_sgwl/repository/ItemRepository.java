1f46befc193257fa750376dc2b695e6990a6662d
Felipe-tecnico-cmyk <felipetecnico911@gmail.com>
2026-09-24 08:25:58 -0300

    Update ItemRepository.java

--- backend/src/main/java/com/aos_sgwl/repository/ItemRepository.java
+++ backend/src/main/java/com/aos_sgwl/repository/ItemRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.aos_sgwl.backend.model.Item;;
-
+//Indica que essa interface atua como um repositório(Comunicação com o banco)
 @Repository
-
+//Estende a interface JpaRepository ao ItemRepository, que fornece métodos CRUD prontos:
+//save(), findById(), findAll(), deleteById(), existsById, entre outros...
+// O primeiro tipo é genérico da entidade(Item), o segundo é o tipo do ID(Long)
 public interface ItemRepository extends JpaRepository<Item, Long>{
     
 }
