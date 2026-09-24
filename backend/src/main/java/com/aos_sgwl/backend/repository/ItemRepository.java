3ddb389445e9936f7a850de3444801dc29e27f89
Felipe-tecnico-cmyk <felipetecnico911@gmail.com>
2026-09-24 08:39:56 -0300

    Documentação(Mapper)

--- backend/src/main/java/com/aos_sgwl/backend/repository/ItemRepository.java
+++ backend/src/main/java/com/aos_sgwl/backend/repository/ItemRepository.java
@@ -1,4 +1,4 @@
-package com.aos_sgwl.repository;
+package com.aos_sgwl.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
