b9759d98e5e4532966238857ab9d60c092ccff47
Felipe-tecnico-cmyk <felipetecnico911@gmail.com>
2026-09-24 08:03:57 -0300

    Repository

--- backend/src/main/java/com/aos_sgwl/backend/controller/ItemController.java
+++ backend/src/main/java/com/aos_sgwl/backend/controller/ItemController.java
@@ -1,11 +1,25 @@
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+package com.aos_sgwl.backend.controller;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+
 @RequestMapping("/api")
+
+@Tag(name = "Item", description = "CRUD operations for item")
+
 public class ItemController {
-    @GetMapping("/itens")
-    public 
+    private final ItemService ItemService;
+
+    public ItemController(ItemService ItemService){
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    
+    
 }
